54075c52a46c80eafdffc91a4eee3c57c7fb598c
farashaeian <z.farashaeian@gmail.com>
2024-02-29 10:15:55 +0330

    show selected products in the cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,18 @@
+import {useContext} from "react";
+import {ShopContext} from "../../context/shopContext";
+import Product from "../shop/product";
 
 const Cart = () => {
+    const { cartItems, addToCart, removeFromCart, products } = useContext(ShopContext);
   return (
     <>
       <h1>Your Cart Items</h1>
+        <div className={'row'}>
+            {products.map((p)=>{
+                if(cartItems.some((i) => i.id === p.id && i.count >0))
+                    return <Product data={p}></Product>
+            })}
+        </div>
     </>
   );
 };
