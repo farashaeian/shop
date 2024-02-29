20e813ff8ced7cd3b3d8e6ba53d290330c3daf33
farashaeian <z.farashaeian@gmail.com>
2024-02-29 11:00:26 +0330

    add selected pruducts number in navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,8 +4,14 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {useContext} from "react";
+import {ShopContext} from "../context/shopContext";
 
 const NavBar = () => {
+    const { cartItems, addToCart, removeFromCart, products } = useContext(ShopContext);
+    const itemCount = cartItems.reduce((prev, current)=>{
+        return prev +current.count
+    },0)
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
@@ -13,7 +19,10 @@ const NavBar = () => {
                     <Navbar.Brand as={Link} to="/">React Shop</Navbar.Brand>
                     <Nav>
                         <Nav.Link as={Link} to="/"><FontAwesomeIcon icon={faShop} /></Nav.Link>
-                        <Nav.Link as={Link} to="/cart"><FontAwesomeIcon icon={faShoppingCart} /></Nav.Link>
+                        <Nav.Link as={Link} to="/cart">
+                            <FontAwesomeIcon icon={faShoppingCart} />
+                            {itemCount > 0 && <span>{itemCount}</span>}
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
